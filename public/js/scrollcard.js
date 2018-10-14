70e30d76671e7cf2ae36a0a5f99b2e1e3b007b1b
neel <neel12@somaiya.edu>
2018-10-14 23:30:14 +0530

    main page upd

---
+++ public/js/scrollcard.js
@@ -0,0 +1,13 @@
+var books = document.getElementsByClassName("isocard__content");
+
+(window).addEventListener("scroll", function () {
+    var travel = (window).scrollY;
+    // console.log(travel);
+    travel = travel/1.5;
+    for(var i=0; i<books.length; i++){
+        books[i].style.transform = 'translate(' +  (i*travel) + 'px,-'+ (i*travel) +'px)';
+        books[i].style.opacity = (i+1)/3;
+    }
+
+
+});
