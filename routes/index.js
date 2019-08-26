33b1bfe920f97605f160639bc09660833c23e226
nilshah98 <neel12@somaiya.edu>
2019-08-26 21:56:19 +0530

    login bug resolved

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post('/register', function(req,res){
       return res.render('register');
     }
     passport.authenticate("local")(req,res,function(){
-      res.render("/lists",{user: req.user});
+      res.redirect("/lists");
     });
   });
 });
